a58543bba48fe58e50b7c66b4ac15cc1e7210a24
pranavz55 <pranavzagade55@gmail.com>
2020-07-01 21:53:51 +0530

    second commit

--- hello.py
+++ hello.py
@@ -1,8 +1,9 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 import json
 
 app = Flask(__name__)
+app.secret_key = 'abcdefghijhi12hikfkn'
 
 
 @app.route('/')
@@ -25,6 +26,7 @@ def your_url():
                 urls = json.load(urls_file)
 
         if request.form['code'] in urls.keys():
+            flash("That short name has already been taken.Please enter another one.")
             return redirect(url_for('home'))
 
         urls[request.form['code']] = {'url': request.form['url']}
@@ -33,3 +35,13 @@ def your_url():
         return render_template('your-url.html', code=request.form['code'])
     else:
         return redirect(url_for('home'))
+
+
+@app.route('/<string:code>')
+def redirect_to_url(code):
+    if os.path.exists('urls.json'):
+        with open('urls.json') as urls_file:
+            urls = json.load(urls_file)
+            if code in urls.keys():
+                if 'url' in urls[code].keys():
+                    return redirect(urls[code]['url'])

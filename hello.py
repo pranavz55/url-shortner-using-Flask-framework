41ec5c75cb3119b0ce3a097abb6712aaf10603d7
pranavz55 <pranavzagade55@gmail.com>
2020-07-01 15:43:36 +0530

    first commit

---
+++ hello.py
@@ -0,0 +1,35 @@
+import os
+from flask import Flask, render_template, request, redirect, url_for
+import json
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+
+@app.route("/login")
+def login():
+    return render_template('home.html')
+
+
+@app.route('/your-url', methods=['GET', 'POST'])
+def your_url():
+    if request.method == 'POST':
+        urls = {}
+
+        if os.path.exists('urls.json'):
+            with open('urls.json') as urls_file:
+                urls = json.load(urls_file)
+
+        if request.form['code'] in urls.keys():
+            return redirect(url_for('home'))
+
+        urls[request.form['code']] = {'url': request.form['url']}
+        with open('urls.json', 'w') as url_file:
+            json.dump(urls, url_file)
+        return render_template('your-url.html', code=request.form['code'])
+    else:
+        return redirect(url_for('home'))
